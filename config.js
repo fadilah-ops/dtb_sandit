142c2ec3e36ecdaeef3e33d020f7a35107ad83e0
fadilah-ops <kurniarizkifadila@gmail.com>
2024-11-28 04:56:35 +0700

    Menyimpan perubahan lokal pada vercel.json

--- config.js
+++ config.js
@@ -1,10 +1,13 @@
-import  { Sequelize }  from "sequelize";
-import dotenv from 'dotenv'
-dotenv.config();
+import { Sequelize } from 'sequelize';
+import mysql2 from 'mysql2'; 
+import dotenv from 'dotenv';
+dotenv.config()
 
-const db = new Sequelize(process.env.DB_NAME,process.env.DB_HOST,process.env.DB_PASSWORD,{
-    host: process.env.HOST,
-    dialect: process.env.DIALECT
-})
+// Konfigurasi koneksi database
+const db = new Sequelize(process.env.DB_NAME,process.env.DB_HOST,process.env.PASSWORD, {
+  host: 'localhost',
+  dialect: 'mysql', // Dialek database
+  dialectModule: mysql2, // Dialek MySQL menggunakan mysql2
+});
 
-export default db;
+export default db; // Gunakan `export` untuk ES Modules
